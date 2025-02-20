0ebb8178ade30c9d45d2600c1a764c6b9cd13fcf
reddooneee <daniele.coppa30@gmail.com>
2025-02-20 12:15:53 +0100

    Initial Commit

---
+++ exprv/src/main/java/com/prenotazioni/exprivia/exprv/ExprvApplication.java
@@ -0,0 +1,13 @@
+package com.prenotazioni.exprivia.exprv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExprvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExprvApplication.class, args);
+	}
+
+}
---
+++ exprv/src/test/java/com/prenotazioni/exprivia/exprv/ExprvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prenotazioni.exprivia.exprv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExprvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
