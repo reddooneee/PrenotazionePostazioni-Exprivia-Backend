07fec41b43f54995f5cc657f6187400d4d697dea
Daniele <danicoppa20@gmail.com>
2025-03-18 13:37:06 +0100

    Aggiunta implementazione per la gestione delle stanze: creazione, aggiornamento e eliminazione tramite StanzeController e StanzeService.

---
+++ backend/exprv/src/main/java/com/prenotazioni/exprivia/exprv/repository/StanzeRepository.java
@@ -0,0 +1,9 @@
+package com.prenotazioni.exprivia.exprv.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.prenotazioni.exprivia.exprv.entity.Stanze;
+
+public interface StanzeRepository extends JpaRepository<Stanze, Integer> {
+
+}
--- backend/exprv/src/main/java/com/prenotazioni/exprivia/exprv/service/PrenotazioniService.java
+++ backend/exprv/src/main/java/com/prenotazioni/exprivia/exprv/service/PrenotazioniService.java
@@ -56,6 +56,7 @@ public class PrenotazioniService {
         return PrenotazioniRepository.save(Prenotazioni);
     }
 
+    //Metodo Per Aggiornare Le Prenotazioni
     public Prenotazioni aggiornaPrenotazioni(Integer id, Prenotazioni Prenotazioni) {
         if (PrenotazioniRepository.existsById(id)) {
             Prenotazioni.setId_prenotazioni(id);
@@ -65,6 +66,7 @@ public class PrenotazioniService {
         }
     }
 
+    //Metodo Per Eliminare le prenotazioni
     public void eliminaPrenotazioni(Integer id) {
         if (PrenotazioniRepository.existsById(id)) {
             PrenotazioniRepository.deleteById(id);
