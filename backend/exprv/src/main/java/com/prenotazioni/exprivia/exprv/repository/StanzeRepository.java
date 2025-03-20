6005418cf9d4d180199956941255e669675239c0
Daniele <daniele.coppa30@gmail.com>
2025-03-20 12:54:30 +0100

    Rimozione Stanze (non entity), nuovo branch
    Rimozione classi di stanze, per metterle nel nuovo branch

--- backend/exprv/src/main/java/com/prenotazioni/exprivia/exprv/repository/StanzeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prenotazioni.exprivia.exprv.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.prenotazioni.exprivia.exprv.entity.Stanze;
-
-public interface StanzeRepository extends JpaRepository<Stanze, Integer> {
-
-}
