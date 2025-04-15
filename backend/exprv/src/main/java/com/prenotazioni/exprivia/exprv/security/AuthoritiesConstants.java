69a998502d08ded5c70fbb1b964ff369184b4779
Kevin Bautista <106398555+henixK@users.noreply.github.com>
2025-04-15 11:43:50 +0200

    Rimozione della costante BUILDM e sistemazione dei commenti nel AuthoritiesConstants

--- backend/exprv/src/main/java/com/prenotazioni/exprivia/exprv/security/AuthoritiesConstants.java
+++ backend/exprv/src/main/java/com/prenotazioni/exprivia/exprv/security/AuthoritiesConstants.java
@@ -1,12 +1,13 @@
 package com.prenotazioni.exprivia.exprv.security;
 
+/**
+ * Constants for Spring Security authorities.
+ */
 public final class AuthoritiesConstants {
 
     public static final String ADMIN = "ROLE_ADMIN";
-
     public static final String USER = "ROLE_USER";
-
-    public static final String BUILDM = "ROLE_BUILDMAN";
+    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {
     }
