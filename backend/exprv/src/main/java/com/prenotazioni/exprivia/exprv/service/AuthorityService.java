062ad8ba3ddc263b61406a0a0a7dbfc9fc42fc52
Daniele <daniele.coppa30@gmail.com>
2025-04-17 16:52:52 +0200

    Aggiunta di un commento per chiarire la funzione di controllo dell'autorità utente nel servizio AuthorityService

--- backend/exprv/src/main/java/com/prenotazioni/exprivia/exprv/service/AuthorityService.java
+++ backend/exprv/src/main/java/com/prenotazioni/exprivia/exprv/service/AuthorityService.java
@@ -1,5 +1,35 @@
 package com.prenotazioni.exprivia.exprv.service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.prenotazioni.exprivia.exprv.security.AuthoritiesConstants;
+
 public class AuthorityService {
+
+    /**
+     * Controllo utente se ha un authority.
+     *
+     * @param authority l'autorità da verificare
+     * @return true se l'utente ha l'autorità, false altrimenti
+     */
+    public static boolean hasAuthority(String authority) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return false;
+        }
+
+        return authentication.getAuthorities().stream()
+                .map(ga -> ga.getAuthority())
+                .anyMatch(auth -> auth.equals(authority));
+    }
+
+    /*
+     * Verifica se l'utente é un admin
+     * @return true se é un adim
+     */
+    public static boolean isAdmin() {
+        return hasAuthority(AuthoritiesConstants.ADMIN);
+    }
 
 }
