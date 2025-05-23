15ec06ede9fde2bbf438e69146382c35d3e0f98c
Daniele <daniele.coppa30@gmail.com>
2025-05-23 16:21:28 +0200

    Aggiornamenti a PostazioniDTO e PostazioniMapper per supportare nuove proprietà e mappature. Modifiche alla configurazione di sicurezza per migliorare la gestione degli endpoint autenticati e pubblici.

--- backend/exprv/src/main/java/com/prenotazioni/exprivia/exprv/dto/PostazioniDTO.java
+++ backend/exprv/src/main/java/com/prenotazioni/exprivia/exprv/dto/PostazioniDTO.java
@@ -1,24 +1,34 @@
 package com.prenotazioni.exprivia.exprv.dto;
 
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.prenotazioni.exprivia.exprv.entity.Stanze;
+//import com.prenotazioni.exprivia.exprv.entity.Stanze;
 import com.prenotazioni.exprivia.exprv.entity.StatoPostazione;
 
 public class PostazioniDTO {
 
     private Integer id_postazione;
-    private Stanze stanze;
+    private String nomePostazione;
+    private Integer id_stanza; // Solo l'ID invece dell'oggetto intero
+    private LocalDateTime creatoIl;
+    private LocalDateTime aggiornatoIl;
 
     @JsonProperty("StatoPostazione")
     private StatoPostazione statoPostazione;
 
+    // Costruttore vuoto
     public PostazioniDTO() {
     }
 
-    public PostazioniDTO(Integer id_postazione, Stanze stanze, StatoPostazione statoPostazione) {
+    // Costruttore con parametri
+    public PostazioniDTO(Integer id_postazione, String nomePostazione, Integer id_stanza, LocalDateTime creatoIl,
+            LocalDateTime aggiornatoIl) {
         this.id_postazione = id_postazione;
-        this.stanze = stanze;
-        this.statoPostazione = statoPostazione;
+        this.nomePostazione = nomePostazione;
+        this.id_stanza = id_stanza;
+        this.creatoIl = creatoIl;
+        this.aggiornatoIl = aggiornatoIl;
     }
 
     public Integer getId_postazione() {
@@ -29,12 +39,36 @@ public class PostazioniDTO {
         this.id_postazione = id_postazione;
     }
 
-    public Stanze getStanze() {
-        return stanze;
+    public String getNomePostazione() {
+        return nomePostazione;
     }
 
-    public void setStanze(Stanze stanze) {
-        this.stanze = stanze;
+    public void setNomePostazione(String nomePostazione) {
+        this.nomePostazione = nomePostazione;
+    }
+
+    public Integer getId_stanza() {
+        return id_stanza;
+    }
+
+    public void setId_stanza(Integer id_stanza) {
+        this.id_stanza = id_stanza;
+    }
+
+    public LocalDateTime getCreatoIl() {
+        return creatoIl;
+    }
+
+    public void setCreatoIl(LocalDateTime creatoIl) {
+        this.creatoIl = creatoIl;
+    }
+
+    public LocalDateTime getAggiornatoIl() {
+        return aggiornatoIl;
+    }
+
+    public void setAggiornatoIl(LocalDateTime aggiornatoIl) {
+        this.aggiornatoIl = aggiornatoIl;
     }
 
     public StatoPostazione getStatoPostazione() {
