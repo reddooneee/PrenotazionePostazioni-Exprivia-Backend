b6ec29e431515186afcb6bf894361e2a8667c35b
Adriano995 <adriano.incognito@icloud.com>
2025-06-05 12:05:24 +0200

    Update Docker
    Correzione comunicazione container docker
    
    Comunicazione funzionante con dati hardcoded
    
    Da implementare dati veri del db vero e continuare a testare
    
    Application-Secrets copiate dentro application-dev per incompatibilità

--- backend/exprv/src/main/java/com/prenotazioni/ExprvApplication.java
+++ backend/exprv/src/main/java/com/prenotazioni/ExprvApplication.java
@@ -3,7 +3,6 @@ package com.prenotazioni;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
 @SpringBootApplication
 @ComponentScan(basePackages = "com.prenotazioni.exprivia.exprv")
 public class ExprvApplication {
--- backend/exprv/src/test/java/com/prenotazioni/exprivia/exprv/ExprvApplicationTests.java
+++ backend/exprv/src/test/java/com/prenotazioni/exprivia/exprv/ExprvApplicationTests.java
@@ -2,8 +2,9 @@ package com.prenotazioni.exprivia.exprv;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = com.prenotazioni.ExprvApplication.class)
 class ExprvApplicationTests {
 
 	@Test
